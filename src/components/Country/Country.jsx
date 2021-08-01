32b628fed87e316972e07f811f6a8e27c6948f08
himubhai20 <himuahmed343@gmail.com>
2021-08-01 12:28:41 +0600

    Cart component added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Country = (props) => {
+    const {name, population, region, flag} = props.country;
+    const divStyle = {border: '1px solid black', margin: '10px', padding: '10px'}
     return (
-        <div>
-            <h3>This is {props.name}</h3>
+        <div style={divStyle}>
+            <h2>This is {name}</h2>
+            <img src= {flag} width="180px" alt='/'></img>
+            <p>Population: <b>{population}</b></p>
+            <p>Region: <b>{region}</b></p>
+            <button onClick={props.handleAddCountry}>Add country</button>
         </div>
     );
 };
